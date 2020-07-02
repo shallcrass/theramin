0dab7290457652885f11e383c13f39a4d3126965
shallcrass <67684611+shallcrass@users.noreply.github.com>
2020-07-02 18:37:57 +0100

    Add files via upload

---
+++ 200621 - Theramin - The input.py
@@ -0,0 +1,18 @@
+# Write your code here :-)
+from gpiozero import DistanceSensor
+from time import sleep
+
+from pythonosc import osc_message_builder
+from pythonosc import udp_client
+
+sensor1 = DistanceSensor(echo=17, trigger=4)
+sensor2 = DistanceSensor(echo=26, trigger=19)
+
+sender = udp_client.SimpleUDPClient('127.0.0.1', 4559)
+
+while True:
+    print (sensor1.distance)
+    print (sensor2.distance)
+    pitch = round(sensor1.distance * 100 +30)
+    sender.send_message('/play_this', pitch)
+    sleep(0.06)
